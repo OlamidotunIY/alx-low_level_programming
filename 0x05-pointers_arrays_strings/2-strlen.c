86fa697483a092290078a5f9350ed716076d5845
OlamidotunIY <olamidotun225@gmail.com>
2023-02-27 07:53:42 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,17 +3,19 @@
 
 /**
  * _strlen = counts the lenght of a string
- * @len = stores the value of the string
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is (success)
  * Return: Always 0.
  */
 
 int _strlen(char *s)
 {
 	int len = 0;
-    while(*s != '\0')
-    {
-        len++;
-        s++;
+	while(*s != '\0')
+	{
+        	len++;
+        	s++;
 	}
-	return len;
+	return (len);
 }
