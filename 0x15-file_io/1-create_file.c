1ca8166c7399946be6ced2fbea5bdc1e2e4e68b6
OlamidotunIY <Olamidotun225@gmail.com>
2023-04-10 02:51:03 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,38 @@
 #include "main.h"
 
 /**
- * create_file - program creates a file
- * @filename: filename.
- * @text_content: content writed in the file.
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of file to append to
+ * @text_content: text to append to file
  *
- * Return: 1 if it success. -1 if it fails.
+ * Return: 1 on success, -1 on failure
  */
-
-int create_file(const char *filename, char *text_content)
-
+int append_text_to_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-		return (-1);
+    int fd, nletters, rwr;
 
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (!filename)
+        return (-1);
 
-	if (fd == -1)
-		return (-1);
+    fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (text_content != NULL)
-	{
-		text_content = "";
-	}
+    if (fd == -1)
+        return (-1);
 
-	for (int nletters = 0; text_content[nletters]; nletters++)
-		;
-	int nletters;
-	int rwr = write(fd, text_content, nletters);
+    if (text_content)
+    {
+        for (nletters = 0; text_content[nletters]; nletters++)
+            ;
 
-	if (rwr == -1)
-		return (-1);
+        rwr = write(fd, text_content, nletters);
 
-	close(fd);
+        if (rwr == -1)
+        {
+            close(fd);
+            return (-1);
+        }
+    }
 
-	return (1);
+    close(fd);
+    return (1);
 }
